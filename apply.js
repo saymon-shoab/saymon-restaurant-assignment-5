92c580527020949dd14490a1b94b9333e80de60f
saymon-shoab <mdsaymonshoab@gmail.com>
2021-02-09 19:04:57 +0600

    problem solved

--- apply.js
+++ apply.js
@@ -1,46 +1,73 @@
-const search = document.getElementById('search');
-const submit = document.getElementById('submit');
-const random = document.getElementById('random');
-const mealsElement = document.getElementById('meals');
-const resultHeading = document.getElementById('result-heading');
-const single_meal = document.getElementById('single-meal');
-
-// search meal from api
-
-function searchMeal(element) {
-    element.preventDefault();
-
-    single_meal.innerHTML='';
-    const term = search.value;
-
-    if(term.trim()){
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-        .then(res => res.json())
-        .then(data => {
-            resultHeading.innerHTML=`
-             <h3>Search result for: ${term}</h3>
-            `;
-            if(data.meal===null){
-                resultHeading.innerHTML=`<p> no result found </p>`
-            }else{
-             mealsElement.innerHTML= data.meals.map(meal => `
-             <div class="meal">
-             <img class="imgSize" src="${meal.strMealThumb}" alt="${meal.strMeal}">
-             <div class="meal-info">
-              <h3>${meal.strMeal}</h3>
-             </div>
-             </div>`
-             )
-             .join('');
-            }
-           
-
-        });
-        search.value = '' ;
-    }else{
-        alert('please enter a search term')
+const searchFood =() =>{
+    const searchText =document.getElementById('input-field').value;
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayMeals(data.meals))
     }
-}
 
-// add eventListeners
-submit.addEventListener('click', searchMeal);
+ const displayMeals = meals=>{
+     const mealsDiv  = document.getElementById('khabar')
+     for (let i = 0; i < meals.length; i++) {
+         const meal = meals[i];
+         const div = document.createElement('div');
+         div.innerHTML=`
+          <div class="meal">
+          <img class="imgSize" src="${meal.strMealThumb}" alt="${meal.strMeal}">
+          <div class="meal-info">
+          <h3>${meal.strMeal}</h3>
+          </div>
+          </div>
+          `;
+         mealsDiv.appendChild(div);
+        
+     }
+ }
+
+
+
+
+
+
+
+
+            // const displayFood = foods =>{
+            // const resultHeading =document.getElementById('result-heading');
+
+            // for(let i=0; i<foods.length ; i++){
+            //  const food=foods[i]
+            // const mealDiv =document.getElementById('meals');
+            // mealDiv.innerHTML=`
+            // <div class="">
+            // <div class="meal">
+            // <img class="imgSize" src="${food.strMealThumb}" alt="${food.strMeal}">
+            // <div class="meal-info">
+            // <h3>${food.strMeal}</h3>
+            // </div>
+            // </div>
+            // </div> `;
+
+            // resultHeading.appendChild(mealDiv);
+
+            // }
+            // }    
+
+
+
+
+
+// foods.forEach(food => {
+//     const mealDiv =document.getElementById('meals');
+//     mealDiv.innerHTML=`
+//     <div class="">
+//       <div class="meal">
+//     <img class="imgSize" src="${food.strMealThumb}" alt="${food.strMeal}">
+//     <div class="meal-info">
+//      <h3>${food.strMeal}</h3>
+//     </div>
+//     </div>
+//      </div> `;
+
+//     resultHeading.appendChild(mealDiv);
+
+//     } );
